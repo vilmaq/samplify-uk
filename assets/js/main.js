25bc5e2d5c8ac1574b70eed66a15868e9205f1f8
Leon <leonwheeler08@gmail.com>
2021-04-08 21:08:04 +0100

    Created a test for the youtube API.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("test");
+const api_key = "AIzaSyDhrIv2axe_DUVDhzFgo9GeFNogHmX3a6w";
+
+async function fetchYoutubeData() {
+  const youtubeUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=the%20weeknd&key=${api_key}`;
+  const response = await fetch(youtubeUrl);
+  const data = await response.json();
+  console.log(data);
+}
