57be41ff63f902c8f3ec0cb4da499146f2372336
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-04-14 21:24:48 +0100

    genre cards array including album artwork and id

---
+++ assets/js/genre-cards.js
@@ -0,0 +1,46 @@
+const genreCards = [
+  {
+    id: 525,
+    albumArtwork: "https://genius.com/album_cover_arts/1733",
+  },
+  {
+    id: 32167,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x440/https%3A%2F%2Fimages.rapgenius.com%2Fe09e829807ee4c2d198736a4d89f0d34.1000x1000x1.jpg",
+  },
+  {
+    id: 2916544,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x440/https%3A%2F%2Fimages.genius.com%2F7aa3bebecdfc0a7f9140c479bcb52182.1000x1000x1.png",
+  },
+  {
+    id: 2263723,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x0/https%3A%2F%2Fimages.genius.com%2Ff3be0158d3a067a81b075686a3a2e63d.1000x1000x1.png",
+  },
+  {
+    id: 2171,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x440/https%3A%2F%2Fimages.genius.com%2Fd2fa9ea19155e704d0300ff0d29e78a3.953x953x1.jpg",
+  },
+  {
+    id: 359249,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x0/https%3A%2F%2Fimages.rapgenius.com%2F0b6e38cbdcd8d33ce9d81b023dd87009.600x600x1.jpg",
+  },
+  {
+    id: 2450584,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x0/https%3A%2F%2Fimages.genius.com%2F5c886671ed0b5bb515ef546e396ef528.1000x1000x1.png",
+  },
+  {
+    id: 3021,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x440/https%3A%2F%2Fimages.genius.com%2Fbb694d2329470f0f08852feacfb5a02a.600x600x1.jpg",
+  },
+  {
+    id: 4249139,
+    albumArtwork:
+      "https://t2.genius.com/unsafe/440x0/https%3A%2F%2Fimages.genius.com%2F4535b464eefdbf847ffc1da7eff2a255.1000x1000x1.png",
+  },
+];
